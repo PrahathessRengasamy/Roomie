3fce7f91bd71f2d9945ada6cf637791b108b282b
Vishal Llewellyn Seshadri <vishal94ls@gmail.com>
2017-04-09 18:25:47 -0400

    before merge

--- Roomie/app/src/main/java/com/example/prahathessrengasamy/roomie/MyFirebaseInstanceIDService.java
+++ Roomie/app/src/main/java/com/example/prahathessrengasamy/roomie/MyFirebaseInstanceIDService.java
@@ -47,6 +47,6 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         //udet u=new udet(token);
         //String user1="Mrunn";
-        mDatabase.child("userdet").child("Mrun").setValue(token);
+        mDatabase.child("userdet").child("vish").setValue(token);
     }
 }
